5271193e8465db60b6c415923bb326c522220c8b
RKCbas <sebashp2015@hotmail.com>
2025-05-06 12:02:15 -0600

    # Class 2 RouterLink ✨Feat: added the router link to the side menu to navigate

--- src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -1,9 +1,37 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOptions {
+  icon: string
+  label: string,
+  route: string,
+  subLabel: string,
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './gifs-side-menu-options.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GifsSideMenuOptionsComponent { }
+export class GifsSideMenuOptionsComponent {
+  
+  menuOptions: MenuOptions[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs populares',
+      route: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar gifs',
+      route: '/dashboard/search'
+    }
+  ]
+  
+}
