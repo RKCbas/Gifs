08c1c0e7af5c4b546d6b2ce57db961930c48d5c5
RKCbas <sebashp2015@hotmail.com>
2025-05-13 19:55:14 -0600

    # Class 14 ✨Feat: Created the logic to detect if the user scrolled at the bottom of tha page

--- src/app/gifs/components/gif-list/gif-list.component.ts
+++ src/app/gifs/components/gif-list/gif-list.component.ts
@@ -11,12 +11,23 @@ import { Gif } from '../../interfaces/gif.interface';
 export class GifListComponent {
   groups = input.required<Gif[][]>(); 
 
-  scrollDivRef = viewChild<ElementRef>('groupDiv')
+  scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv')
 
   onScroll( event:Event ){
     const scrollDiv = this.scrollDivRef()?.nativeElement;
 
-    // console.log(scrollDiv);
+    if ( !scrollDiv )return;
+
+    const scrollTop = scrollDiv.scrollTop;
+    const clientHeight = scrollDiv.clientHeight;
+    const scrollHeight = scrollDiv.scrollHeight;
+
+    const isAtBottom = scrollTop+clientHeight >= scrollHeight*0.85;
+    
+    if(isAtBottom){
+      //TODO Logic to charge more gifs
+    }
+
   }
 
 }
